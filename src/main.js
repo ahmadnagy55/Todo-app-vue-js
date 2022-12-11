cb7a5be70fd3caa680fa41d940cf74b23bfdd3fb
Ahmad Nagy <ahmadnagy55@gmail.com>
2022-12-11 19:53:28 +0200

    edit some cdn to fontawesome path

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
-import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import "./main.css";
 
 createApp(App).use(router).mount("#app");
